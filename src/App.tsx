7b5aea721bbe953621308166622777ccd6992c86
krishna <krishnaprasad@cstechcs.com>
2025-06-30 14:01:05 +0530

    path fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Footer from './components/Footer';
-import Navbar from './components/Navbar';
-import AboutPage from './pages/AboutPage';
-import ContactPage from './pages/ContactPage';
-import HomePage from './pages/HomePage';
-import ProjectsPage from './pages/ProjectsPage';
-import SinglePage from './pages/SinglePage';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
+import HomePage from "./pages/HomePage";
+import ProjectsPage from "./pages/ProjectsPage";
+import SinglePage from "./pages/SinglePage";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className='min-h-screen bg-gradient-to-br from-dark-950 via-dark-900 to-dark-800 text-white'>
+      <div className="min-h-screen bg-gradient-to-br from-dark-950 via-dark-900 to-dark-800 text-white">
         <Navbar />
         <main>
           <Routes>
-            <Route path='/' element={<HomePage />} />
-            <Route path='/about' element={<AboutPage />} />
-            <Route path='/projects' element={<ProjectsPage />} />
-            <Route path='/contact' element={<ContactPage />} />
-            <Route path='/single-page' element={<SinglePage />} />
+            <Route path="/protfolio-rk/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/projects" element={<ProjectsPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="/single-page" element={<SinglePage />} />
           </Routes>
         </main>
         <Footer />
